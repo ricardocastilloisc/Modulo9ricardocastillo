748e8cc31f67cf57f119d0887d30876fd2fd4e9b
ricardocastilloisc <ricardocastilloisc@gmail.com>
2015-08-11 08:58:23 -0500

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,13 @@
+// MW de autorizacion de acceso HTTP restringidos
+exports.loginRequired = function(req,res,next){
+    if(req.session.user){
+        next();
+    }else{
+        res.redirect('/login');
+    }
+};
+
+
 // Get /login ---Formulario de login
 exports.new = function(req,res){
     var errors = req.session.errors || {};
